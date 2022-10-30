61cd4eaf62fbbd5979064fccefff71100a6adfb9
Théo HEMMER <theo.hemmer@epitech.eu>
2022-10-30 11:52:22 +0000

    Changes for custom generator

--- pldGenerator/custom/customGenerator/customPldGenerator.js
+++ pldGenerator/custom/customGenerator/customPldGenerator.js
@@ -1,4 +1,4 @@
-module.exports = function generatePld(cards) {
+module.exports.generatePld = function generatePld(cards) {
     /**
      * You get the cards here, you can generate the PLD as you want with them
      * The module used to generate the PDF is pdfmake, you might want to take a look at their documentation
