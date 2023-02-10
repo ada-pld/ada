db11bf623fcbe022afc198a89bb28b8efcf8900c
Théo HEMMER <theo.hemmer@epitech.eu>
2023-02-10 18:14:26 +0000

    Add a not-tested sprintController

---
+++ src/apiControllers/sprintController.ts
@@ -0,0 +1,162 @@
+import IController from "../controllers/controller";
+import express, {Request, Response} from "express";
+import { checkPerm, checkPermAPI } from "../middlewares/checkPerms";
+import { authBearer, authUser } from "../middlewares/auth";
+import Part from "../models/part";
+import Card from "../models/card";
+import Sprint from "../models/sprint";
+import User from "../models/user";
+
+class SprintController implements IController {
+    public path = "/sprint";
+    public router = express.Router();
+
+    constructor() {
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes() {
+        this.router.get("/active", authBearer, this.active);
+        this.router.get("/cardsStats", authBearer, checkPermAPI("MAINTENER"), this.cardsStats);
+        this.router.get("/list", authBearer, checkPermAPI("EDITOR"), this.listSprint);
+        this.router.get("/use/:sprint", authBearer, checkPermAPI("EDITOR"), this.useSprint);
+        this.router.post("/create", authBearer, checkPermAPI("EDITOR"), this.createSprint);
+    }
+
+    private active = async (req: Request, res: Response) => {
+        return res.status(200).send(req.wap.sprint);
+    }
+
+    private cardsStats = async (req: Request, res: Response) => {
+        let allUsers = await User.findAll({
+            order: [['firstname', 'ASC'], ['lastname', 'ASC']],
+            include: [
+                {
+                    model: Card,
+                    where: {
+                        sprintId: req.wap.sprint.id
+                    },
+                    include: [
+                        Card,
+                        User
+                    ]
+                }
+            ]
+        })
+        allUsers = allUsers.map(x => x.toJSON());
+
+        for (const user of allUsers) {
+            const injected = user as (User & {
+                JHIntended: number, 
+                JHDones: number,
+                JHInProgress: number,
+                JHNotStarted: number,
+                JHNotAccepted: number,
+                JHMissing: number,
+            })
+            injected.JHIntended = 0;
+            injected.JHDones = 0;
+            injected.JHInProgress = 0;
+            injected.JHNotStarted = 0;
+            injected.JHNotAccepted = 0;
+            injected.JHMissing = 0;
+            for (const card of user.cards) {
+                if (card.status != "REJECTED" && card.status != "FINISHED") {
+                    injected.JHIntended += (card.workingDays / card.assignees.length);
+                } else {
+                    injected.JHNotAccepted += (card.workingDays / card.assignees.length);
+                }
+                if (card.status == "FINISHED") {
+                    injected.JHDones += (card.workingDays / card.assignees.length);
+                } else if (card.status == "STARTED") {
+                    injected.JHInProgress += (card.workingDays / card.assignees.length);
+                } else if (card.status == "NOT_STARTED") {
+                    injected.JHNotStarted += (card.workingDays / card.assignees.length);
+                }
+            }
+            injected.JHMissing = req.wap.sprint.workDaysNeeded - injected.JHIntended;
+        }
+        return res.status(200).send(allUsers);
+    }
+
+    private listSprint = async (req: Request, res: Response) => {
+        const allSprints = await Part.findAll({
+            include: [
+                Card
+            ]
+        });
+        
+        allSprints.forEach((x) => {
+            const resultObject = x.toJSON() as (Sprint & {totalCards: number});
+            resultObject.totalCards = x.cards.filter(x => !["REJECTED", "WAITING_APPROVAL"].includes(x.status)).length;
+        });
+
+        return res.status(200).send(allSprints);
+    }
+
+    private useSprint = async (req: Request, res: Response) => {
+        const sprint = await Sprint.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!sprint) {
+            return res.status(400).send({
+                message: "Invalid id"
+            });
+        }
+        const sprints = await Sprint.findAll();
+        let allPromises = [];
+        sprints.forEach((x) => {
+            x.active = false;
+            if (sprint.id == req.params.id as any) {
+                x.active = true
+            }
+            allPromises.push(new Promise(async (resolve, reject) => {
+                await x.save();
+                resolve(true);
+            }))
+        })
+        await Promise.all(allPromises);
+        req.wap.sprint = sprint;
+        return res.status(200).send({
+            message: "Success."
+        });
+    }
+
+    private createSprint = async (req: Request, res: Response) => {
+        if (!req.body.name || !req.body.workDaysNeeded) {
+            return res.status(400).send({
+                message: "Invalid body."
+            });
+        }
+        let value :number = 0;
+        try {
+            value = parseInt(req.body.workDaysNeeded);
+        } catch (e) {
+            return res.status(400).send({
+                message: "Invalid workDays."
+            })
+        }
+        const alreadyExists = await Sprint.findOne({
+            where: {
+                name: req.body.name
+            }
+        });
+        if (alreadyExists) {
+            return res.status(400).send({
+                message: "Name already in use."
+            });
+        }
+        await Sprint.create({
+            name: req.body.name,
+            workDaysNeeded: value
+        });
+        return res.status(200).send({
+            message: "Success."
+        });
+    }
+
+}
+
+export default SprintController;
